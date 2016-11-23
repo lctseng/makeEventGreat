e5c8f2d2b05bd2905ff1cf3e54c86a122a575154
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2016-11-24 00:20:45 +0800

    set default encoding as UTF-8

--- external/classifier.rb
+++ external/classifier.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require "json"
 
+Encoding.default_external = "utf-8"
+Encoding.default_internal = "utf-8"
+
 data = JSON.parse($stdin.read.encode("utf-8").gsub(/[\t]+/,' '))
 data.each do |event|
   event["type"] ||= []
